c0c28d510c7e8492dde72dff41d6707c48182221
Shoko Shirai <68937284+shokoshirai@users.noreply.github.com>
2025-10-31 14:34:48 +0900

    cache clear

--- .docusaurus/client-modules.js
+++
@@ -1,9 +0,0 @@
-export default [
-  require("/Users/mitubacal/github/lunascape-mobile-user-docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/mitubacal/github/lunascape-mobile-user-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/mitubacal/github/lunascape-mobile-user-docs/node_modules/@docusaurus/preset-classic/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/mitubacal/github/lunascape-mobile-user-docs/node_modules/@docusaurus/preset-classic/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/mitubacal/github/lunascape-mobile-user-docs/src/css/custom.css"),
-  require("/Users/mitubacal/github/lunascape-mobile-user-docs/src/css/hubspot-form.css"),
-  require("/Users/mitubacal/github/lunascape-mobile-user-docs/node_modules/docusaurus-plugin-image-zoom/lib/zoom"),
-];
